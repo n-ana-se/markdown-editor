283a62a7ee9bb0e88b7dfee1a242607e5ff561d3
n-ana-se <sudo.kazuki.0709@icloud.com>
2021-09-19 18:36:26 +0900

    3回目のコミット

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.tsx$/,
+                test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
@@ -18,5 +18,13 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
         publicPath: 'dist/'
-    }
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        hot: true,
+        open: true,
+    },
+    mode: 'production'
 }
